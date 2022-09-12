18cc3a8dc6ddd6dfcd4ca840591fdbf49bfc44da
Vinay Kumar Srivastava <vinaysrivastava273@gmail.com>
2022-09-12 14:04:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -82,18 +82,7 @@ function showWeatherData (data){
         }
     })
     weatherForecastEl.innerHTML = otherDayForcast;
-    
-    document.querySelector(".temp").innerText = temp + "°C";
-    document.querySelector(".city").innerText = "Weather in " + name;
-    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector(".description").innerText = description;
-    document.querySelector(".humidity").innerText =
-      "Population: " + population;
-    document.querySelector(".wind").innerText =
-      "Sea-level: " + sea_level;
-    document.querySelector(".weather").classList.remove("loading");
-    document.body.style.backgroundImage =
-      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+
 }
 
 function getWeatherData ( city ) {
@@ -135,17 +124,12 @@ let weather = {
         const { speed } = data.list[0].wind;
 
         document.querySelector(".city").innerText = "Weather in " + name;
-        document.querySelector(".icon").src =
-        "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C";
-        document.querySelector(".humidity").innerText =
-        "Humidity: " + humidity + "%";
-        document.querySelector(".wind").innerText =
-        "Wind speed: " + speed + " km/h";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
         document.querySelector(".weather").classList.remove("loading");
-        document.body.style.backgroundImage =
-        "url('https://source.unsplash.com/1600x900/?" + name + "')";
     },
     search: function () {
         this.fetchWeather(document.querySelector(".search-bar").value);
